1a2cb7eefc9faa03cd158e1e2542e0ed3638dec4
leva <leonid.lukyanov@persgroep.net>
2025-03-04 23:21:29 +0200

    redirecting to the thankyou page after subbmission

--- gulp/src/js/custom.js
+++ gulp/src/js/custom.js
@@ -1,42 +1,48 @@
-$(document).ready(function (){
-    $(".maf_element").on('click', function (){
+$(document).ready(function () {
+    $(".maf_element").on('click', function () {
         let dataRequest = {
             action: 'open_details',
             maf_id: $(this).data('id'),
         };
 
-            $.ajax({
-                type: "POST",
-                url: '/wp-admin/admin-ajax.php',
-                data: dataRequest,
-                beforeSend : function(){
-                },
-                success: function (data) {
-                    if (data.success) {
-                       $('#popup-details .content').html(data.html);
-                       $('#popup-details').show();
-                    }
+        $.ajax({
+            type: "POST",
+            url: '/wp-admin/admin-ajax.php',
+            data: dataRequest,
+            beforeSend: function () {
+            },
+            success: function (data) {
+                if (data.success) {
+                    $('#popup-details .content').html(data.html);
+                    $('#popup-details').show();
                 }
-            });
+            }
+        });
     })
 
-    $("#popup_details .close").on('click', function (){
+    $("#popup_details .close").on('click', function () {
         $('#popup-details').hide();
     })
 
 
-
-    $('.header__burger').click(function(event) {
-        $('.header__burger').toggleClass('active'); 
-        $('.header').toggleClass('active'); 
-        $('body').toggleClass('hidden'); 
-        $('.main_nav').slideToggle();  
+    $('.header__burger').click(function (event) {
+        $('.header__burger').toggleClass('active');
+        $('.header').toggleClass('active');
+        $('body').toggleClass('hidden');
+        $('.main_nav').slideToggle();
     });
 
-    $("body").on("click",".link-to", function (event) {
+    $("body").on("click", ".link-to", function (event) {
         event.preventDefault();
         var id = $(this).attr('href'),
-        top = $(id).offset().top;
+            top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 900);
-      });
+    });
+
+    document.addEventListener( 'wpcf7mailsent', function( event ) {
+        setTimeout(() => {
+            location = '/thank-you';
+        }, 300); // Wait for 3 seconds to redirect.
+    }, false );
+
 })
